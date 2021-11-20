073b7d27f3da716d518152e0e8816ab782736880
moinabyssinia <michaelg.tadesse@gmail.com>
2021-11-20 17:21:48 -0500

    adding models

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   # get requests for the /pages path should go to
   # the PagesController's index method 
   get '/pages', to: 'pages#index'
+
+  # to get the form
+  get '/pages/new', to: 'pages#new', as: 'new_page'
+
+  get '/pages/:id', to: 'pages#show', as: 'page'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
