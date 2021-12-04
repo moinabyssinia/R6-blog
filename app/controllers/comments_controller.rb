51aea22f9ae804f9b6b200e8c0be16a9067d1b87
moinabyssinia <michaelg.tadesse@gmail.com>
2021-12-04 17:28:11 -0500

    delete option

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,12 @@ class CommentsController < ApplicationController
         redirect_to @post
     end
 
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy 
+        redirect_to @post        
+    end
+
     private 
         def comment_params
             params.require(:comment).permit(:name, :content)
